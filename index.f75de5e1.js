840634a9f41404b691149b49de6392a4e3a4a7b3
dima <dima98doroshenko@gmail.com>
2024-09-14 19:56:25 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const wallWitdh = wall.clientWidth;
+const wallHeight = wall.clientHeight;
+const spiderWidth = spider.clientWidth;
+const spiderHeight = spider.clientHeight;
+spider.style.top = `${wallHeight / 2 - spiderHeight / 2}px`;
+spider.style.left = `${wallWitdh / 2 - spiderWidth / 2}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
